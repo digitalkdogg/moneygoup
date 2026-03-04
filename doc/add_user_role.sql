47c73e5c873e9cb5136c48be2d3b8782f7ff3122
Kevin Bollman <digitalkdogg@gmail.com>
2026-03-03 20:00:41 -0600

    Updated documentation

--- doc/add_user_role.sql
+++
@@ -1,5 +0,0 @@
--- Migration to add role column to users table
-ALTER TABLE `users` ADD COLUMN `role` ENUM('user', 'premium', 'admin') NOT NULL DEFAULT 'user';
-
--- Optional: Set an initial admin (replace 'admin_username' with actual username if known)
--- UPDATE `users` SET `role` = 'admin' WHERE `username` = 'admin_username';
